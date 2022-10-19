70fa6021f970375619edd0cde5e0c3d8887e0f80
Livinus <110895424+mirapeak@users.noreply.github.com>
2022-10-19 05:22:02 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -53,7 +53,7 @@ int is_digit(char c)
 
 /**
  * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
+ * @num: Number to be casted
  * @size: Number indicating the type to be casted.
  *
  * Return: Casted value of num
